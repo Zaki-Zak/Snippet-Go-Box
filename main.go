297ccbb8354dcfff7752e7e9fba0566638b6b9cc
MihoZaki <mihoubiadam7@gmail.com>
2025-01-21 18:56:38 +0100

    start mux server on :4000

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from the server "))
+}
 
 func main() {
-	fmt.Println("hello world!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	log.Println("starting ther server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
