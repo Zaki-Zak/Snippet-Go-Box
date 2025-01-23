2c3d02dd6f92ad552a78851ee1e107332836d7f2
MihoZaki <mihoubiadam7@gmail.com>
2025-01-23 11:27:54 +0100

    revisit wild cards

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
+	w.Write([]byte(msg))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from snippetCreate"))
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from the server "))
@@ -11,7 +27,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view/{id}", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 	log.Println("starting ther server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
