96d009fe735128014c1c0a449d562c805620f0a3
MihoZaki <mihoubiadam7@gmail.com>
2025-01-26 13:21:03 +0100

    Add connection to the database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
+	"log"
 	"log/slog"
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql" // New import
+	"github.com/joho/godotenv"
 )
 
 type application struct {
@@ -39,17 +46,57 @@ func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
 	return f, nil
 }
 
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to datebase: %w", err)
+	}
+	// Set connection pool settings
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(25)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	// Verify the connction
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, fmt.Errorf("failed to ping datebase: %w", err)
+	}
+	return db, nil
+}
+
+func getDefaultDSN() string {
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbParseTime := os.Getenv("DB_PARSE_TIME")
+	return fmt.Sprintf("%s:%s@/%s?parseTime=%s", dbUser, dbPassword, dbName, dbParseTime)
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP nerwork address")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dsn := flag.String("dsn", getDefaultDSN(), "MySQL data source name")
+
 	flag.Parse()
 
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
 	app := &application{
 		logger: logger,
 	}
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error("failed to open database", "dbError", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
 	logger.Info("starting server on", "addr", *addr)
-	err := http.ListenAndServe(*addr, app.routes())
-	logger.Error(err.Error())
+	err = http.ListenAndServe(*addr, app.routes())
+	logger.Error("server error", "error", err)
 	os.Exit(1)
 }
